27d238a18b6aa7d7056321cc8196dec133a97438
Vincent Cui <vincenthcui@qq.com>
2021-02-13 02:33:30 +0800

    expose client mutate method

--- tencentcloud/option.go
+++ tencentcloud/option.go
@@ -2,6 +2,10 @@ package tencentcloud
 
 type Option func(*Client)
 
+func (c *Client) Mutate(option Option) {
+	option(c)
+}
+
 func WithSecret(id, key string) Option {
 	return func(c *Client) {
 		c.secretID = id
