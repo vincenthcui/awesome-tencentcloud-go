515c5d4e713feedd35f9885413968892f58c7b6f
vincentcui <vincentcui@tencent.com>
2021-02-19 15:41:53 +0800

    add actions

--- tencentcloud/action.go
+++ tencentcloud/action.go
@@ -4,34 +4,26 @@ package tencentcloud
 type Action interface {
 	// Service name, e.g. CVM/CDB/CLB
 	Service() string
-	// Action name, e.g. DescribeInstances
-	Action() string
 	// Version of api, e.g. 2017-03-12
 	Version() string
+	// Action name, e.g. DescribeInstances
+	Action() string
 }
 
 type action struct {
 	service string
-	action  string
 	version string
+	action  string
 }
 
 func (act action) Service() string {
 	return act.service
 }
 
-func (act action) Action() string {
-	return act.action
-}
-
 func (act action) Version() string {
 	return act.version
 }
 
-var (
-	CVMDescribeInstances = action{
-		service: "cvm",
-		action:  "DescribeInstances",
-		version: "2017-03-12",
-	}
-)
+func (act action) Action() string {
+	return act.action
+}
---
+++ tencentcloud/action_generated.go
@@ -0,0 +1,26 @@
+package tencentcloud
+
+// suppose to auto-generate action list here
+
+var (
+	CVMDescribeInstances = action{
+		service: "cvm",
+		version: "2017-03-12",
+		action:  "DescribeInstances",
+	}
+	CVMDescribeImages = action{
+		service: "cvm",
+		version: "2017-03-12",
+		action:  "DescribeImages",
+	}
+	CVMDescribeInstanceTypeConfigs = action{
+		service: "cvm",
+		version: "2017-03-12",
+		action:  "DescribeInstanceTypeConfigs",
+	}
+	TAGModifyResourcesTagValue = action{
+		service: "tag",
+		version: "2018-08-13",
+		action:  "ModifyResourcesTagValue",
+	}
+)
