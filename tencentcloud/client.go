14fa13b65f60ed9f144406886ea3a021a067f889
vincentcui <vincentcui@tencent.com>
2021-02-20 10:21:07 +0800

    IgnoreResponse 提供指针

--- tencentcloud/client.go
+++ tencentcloud/client.go
@@ -164,6 +164,6 @@ func (c Client) Region() string {
 }
 
 // IgnoreResponse return anonymous map to ignore reponse
-func IgnoreResponse() map[string]interface{} {
-	return map[string]interface{}{}
+func IgnoreResponse() *map[string]interface{} {
+	return &map[string]interface{}{}
 }
