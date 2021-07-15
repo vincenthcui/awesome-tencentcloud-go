7f2f039aa592d59fc541700699b6cb3d9c4987ad
vincentcui <vincentcui@tencent.com>
2021-07-15 11:46:12 +0800

    fix metrics name

--- tencentcloud/metrics/metrics.go
+++ tencentcloud/metrics/metrics.go
@@ -6,23 +6,23 @@ import (
 
 var (
 	RequestTotal = prom.NewCounterVec(
-		prom.CounterOpts{Subsystem: "awesome-tencentcloud-client", Name: "request_total"},
+		prom.CounterOpts{Subsystem: "awesome_tencentcloud_client", Name: "request_total"},
 		[]string{"sid", "service", "action", "version"},
 	)
 	ErrorTotal = prom.NewCounterVec(
-		prom.CounterOpts{Subsystem: "awesome-tencentcloud-client", Name: "error_total"},
+		prom.CounterOpts{Subsystem: "awesome_tencentcloud_client", Name: "error_total"},
 		[]string{"sid", "service", "action", "version", "code"},
 	)
 	NetworkFailureRetryTotal = prom.NewCounterVec(
-		prom.CounterOpts{Subsystem: "awesome-tencentcloud-client", Name: "network_failure_retry_total"},
+		prom.CounterOpts{Subsystem: "awesome_tencentcloud_client", Name: "network_failure_retry_total"},
 		[]string{"service", "action", "version"},
 	)
 	RateLimitRetryTotal = prom.NewCounterVec(
-		prom.CounterOpts{Subsystem: "awesome-tencentcloud-client", Name: "rate_limit_retry_total"},
+		prom.CounterOpts{Subsystem: "awesome_tencentcloud_client", Name: "rate_limit_retry_total"},
 		[]string{"service", "action", "version"},
 	)
 	RequestDuration = prom.NewGaugeVec(
-		prom.GaugeOpts{Subsystem: "awesome-tencentcloud-client", Name: "request_duration"},
+		prom.GaugeOpts{Subsystem: "awesome_tencentcloud_client", Name: "request_duration"},
 		[]string{"service", "action", "version"},
 	)
 )
